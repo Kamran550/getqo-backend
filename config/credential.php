d0e050049913c9eb3ef836d3216a57c1a2168a27
root <root@164-92-134-149.cprapid.com>
2025-03-13 10:58:21 +0400

    İlk backend commit

---
+++ config/credential.php
@@ -0,0 +1,8 @@
+<?php
+ return [
+                        
+'purchase_id' => '43679000',
+                            
+'purchase_code' => 'f7792e3d-c05c-4615-81c0-783ff623f47d',
+                        
+];
